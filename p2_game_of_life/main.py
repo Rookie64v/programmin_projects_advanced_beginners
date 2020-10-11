61872afb77864dcdf103d11d052c4fdfadff445d
Flavio Tanese <flavio.tanese@gmail.com>
2020-10-11 15:01:40 +0200

    Changed string representation of board, added next state logic and main loop

--- p2_game_of_life/main.py
+++ p2_game_of_life/main.py
@@ -20,18 +20,53 @@ class Board:
                 self.state[row][col] = bits_of_row & 1
                 bits_of_row >>= 1
 
+    def next_state(self) -> None:
+        next_state = self.state.copy()
+        for row_idx in range(len(next_state)):
+            for col_idx in range(len(next_state[0])):
+                alive_neighbours = self._get_alive_neighbours(row_idx, col_idx)
+                if alive_neighbours <= 1 or alive_neighbours > 3:
+                    next_state[row_idx][col_idx] = 0
+                # alive_neighbours == 2 keeps value, that was done by copy()
+                elif alive_neighbours == 3:
+                    next_state[row_idx][col_idx] = 1
+        self.state = next_state
+
+    def _get_alive_neighbours(self, row_idx: int, col_idx: int) -> int:
+        alive_neighbours = 0
+        for i in (row_idx-1, row_idx, row_idx+1):
+            if i < 0 or i >= self.height:
+                continue
+            for j in (col_idx-1, col_idx, col_idx+1):
+                if j < 0 or j >= self.width:
+                    continue
+                if (i, j) == (row_idx, col_idx):
+                    continue
+                if self.state[i][j]:
+                    alive_neighbours += 1
+        return alive_neighbours
+
     def __str__(self):
         s = ''
+        horizontal_border = '--' * len(self.state[0])
+        corner = '+'
+        vertical_border_bit = '|'
+        s += corner + horizontal_border + corner + '\n'
         for row in self.state:
+            s += vertical_border_bit
             for bit in row:
-                s += f'{bit} '
-            s = s.rstrip()
-            s += '\n'
-        s = s.rstrip()
+                s += f'{"##" if bit else "  "}'
+            s += vertical_border_bit + '\n'
+        s += corner + horizontal_border + corner + '\n'
         return s
 
 
 if __name__ == '__main__':
-    board = Board(width=10, height=5)
+    import time
+    board = Board(width=20, height=20)
     board.randomize()
     print(board)
+    for i in range(100):
+        board.next_state()
+        print(board)
+        time.sleep(0.3)
